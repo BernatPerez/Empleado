1844b06c447ae74884f72af4818187897bf5cae2
Bernat <Email bueno>
2021-01-10 19:07:36 +0100

    Javadoc Generation

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,3 +1,8 @@
+/**
+ * Clase Empleado contiene información de cada empleado
+ * @version 15.11
+ * @author Bernat Pérez, <a href= "mailto:perezroig.bernat@mail.com"> mail me </a> 
+ */
 public class Empleado {
   private String nombre;
   private String apellido;
@@ -5,6 +10,16 @@ public class Empleado {
   private double salario;
   
   //Metodos publicos
+  /**
+   * Suma un plus de XX euros al salario del empleado si el empleado tiene mas de ZZ años
+omite esta línea (sustituye la XX por tu año de nacimiento y la ZZ por tu edad )
+omite esta línea (Si naciste en el 80 y tu edad es de 40 años sería:...)
+   * @param sueldoPlus  Cantidad en euros que se suma al salario
+   * @return <ul> 
+   *        <li> True: Se suma el plus al sueldo </li>
+   *        <li> False: No se suma el plus al sueldo </li>
+   *        </ul>
+   */
   public boolean plus (double sueldoPlus){
     boolean aumento=false;
     if (edad>40 && compruebaNombre()){
@@ -21,12 +36,21 @@ public class Empleado {
     }
     return true;
   }
- 
+ /**
+  * Constructor por defecto
+  */
   //Constructores
   public Empleado(){
     this ("", "", 0, 0);
   }
-   
+  /**
+   * Constructor con 4 parámetros
+   * 
+   * @param nombre Nombre del empleado
+   * @param apellido nombre del empleado
+   * @param edad edad del empleado
+   * @param salario salario del empleado
+   */
   public Empleado(String nombre, String apellido, int edad, double salario){
     this.nombre=nombre;
     this.apellido=apellido;
